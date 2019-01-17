65399795db90eb2ecf2e8b7d8fe09fc5b4158551
George McCarron <hello@georgemccarron.com>
2019-01-17 11:22:06 +0000

    loads book data to LinkedList

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book("Harry Potter", "J.K. Rowling", 30, 750);
-        System.out.println(book1.getMaxLoanPeriod()); // '30'
-        System.out.println(book1.isAvailable()); // 'true'
-        book1.setAvailable(false);
-        System.out.println(book1.isAvailable()); // 'false'
+        LinkedList<LoanableItem> items = new LinkedList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("books.csv"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] split = line.split(",");
+                if (split.length > 1) {
+                    String author = split[0];
+                    String title = split[1];
+                    items.add(new Book(title, author, 30, ((int) Math.random() * 1500) + 1));
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Error reading the book data!");
+        }
     }
 }
