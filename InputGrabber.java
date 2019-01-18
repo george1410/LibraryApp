e3f8fe0513d5e54506d232ec320578c37380b251
George McCarron <hello@georgemccarron.com>
2019-01-18 17:19:20 +0000

    changed InputGrabber to avoid code repetition

--- InputGrabber.java
+++ InputGrabber.java
@@ -4,14 +4,16 @@ public class InputGrabber {
     static Scanner kb = new Scanner(System.in);
 
     public String grab() {
-        String input = "";
-        input = kb.nextLine();
-        return input;
+        return readInput();
     }
 
     public String grab(String prompt) {
-        String input = "";
         System.out.print(prompt);
+        return readInput();
+    }
+
+    private String readInput() {
+        String input = "";
         input = kb.nextLine();
         return input;
     }
